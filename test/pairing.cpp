d30221c46f590d08a7254e3b7065784d47367723
Xu Cheng <git@xuc.me>
2016-09-26 15:49:48 +0800

    add test for pairing info

--- test/pairing.cpp
+++ test/pairing.cpp
@@ -1,11 +1,34 @@
 #include "helper.h"
 
+struct Fixture {
+    Fixture() { param = PairingParam::gen_type_a(160, 512); }
+    ~Fixture() {}
+    PairingParamPtr param;
+};
+
+BOOST_FIXTURE_TEST_SUITE(param, Fixture)
+
 BOOST_AUTO_TEST_CASE(init_from_param_and_str)
 {
-    PairingParamPtr param = PairingParam::gen_type_a();
     PairingPtr pariring1 = Pairing::init_from_param(param);
     PairingPtr pariring2 = Pairing::init_from_param_str(param->to_str());
     BOOST_TEST(pariring1->symmetric() == pariring2->symmetric());
     BOOST_TEST(pariring1->g1_bytes_length() == pariring2->g1_bytes_length());
     BOOST_TEST(pariring1->g2_bytes_length() == pariring2->g2_bytes_length());
 }
+
+BOOST_AUTO_TEST_CASE(pairing_info)
+{
+    PairingPtr pariring = Pairing::init_from_param(param);
+    BOOST_TEST(pariring->symmetric() == true);
+    BOOST_TEST(pariring->g1_bytes_length() == 128);
+    BOOST_TEST(pariring->g1_x_only_bytes_length() == 64);
+    BOOST_TEST(pariring->g1_compressed_bytes_length() == 65);
+    BOOST_TEST(pariring->g2_bytes_length() == 128);
+    BOOST_TEST(pariring->g2_x_only_bytes_length() == 64);
+    BOOST_TEST(pariring->g2_compressed_bytes_length() == 65);
+    BOOST_TEST(pariring->gt_bytes_length() == 128);
+    BOOST_TEST(pariring->zr_bytes_length() == 20);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
