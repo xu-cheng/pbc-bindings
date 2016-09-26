4aea5f86c4c7b1c45573a6b97ab9cf999065f7c8
Xu Cheng <git@xuc.me>
2016-09-26 18:27:48 +0800

    fix typo

--- test/pairing.cpp
+++ test/pairing.cpp
@@ -10,25 +10,25 @@ BOOST_FIXTURE_TEST_SUITE(param, Fixture)
 
 BOOST_AUTO_TEST_CASE(init_from_param_and_str)
 {
-    PairingPtr pariring1 = Pairing::init_from_param(param);
-    PairingPtr pariring2 = Pairing::init_from_param_str(param->to_str());
-    BOOST_TEST(pariring1->symmetric() == pariring2->symmetric());
-    BOOST_TEST(pariring1->g1_bytes_length() == pariring2->g1_bytes_length());
-    BOOST_TEST(pariring1->g2_bytes_length() == pariring2->g2_bytes_length());
+    PairingPtr pairing1 = Pairing::init_from_param(param);
+    PairingPtr pairing2 = Pairing::init_from_param_str(param->to_str());
+    BOOST_TEST(pairing1->symmetric() == pairing2->symmetric());
+    BOOST_TEST(pairing1->g1_bytes_length() == pairing2->g1_bytes_length());
+    BOOST_TEST(pairing1->g2_bytes_length() == pairing2->g2_bytes_length());
 }
 
 BOOST_AUTO_TEST_CASE(pairing_info)
 {
-    PairingPtr pariring = Pairing::init_from_param(param);
-    BOOST_TEST(pariring->symmetric() == true);
-    BOOST_TEST(pariring->g1_bytes_length() == 128);
-    BOOST_TEST(pariring->g1_x_only_bytes_length() == 64);
-    BOOST_TEST(pariring->g1_compressed_bytes_length() == 65);
-    BOOST_TEST(pariring->g2_bytes_length() == 128);
-    BOOST_TEST(pariring->g2_x_only_bytes_length() == 64);
-    BOOST_TEST(pariring->g2_compressed_bytes_length() == 65);
-    BOOST_TEST(pariring->gt_bytes_length() == 128);
-    BOOST_TEST(pariring->zr_bytes_length() == 20);
+    PairingPtr pairing = Pairing::init_from_param(param);
+    BOOST_TEST(pairing->symmetric() == true);
+    BOOST_TEST(pairing->g1_bytes_length() == 128);
+    BOOST_TEST(pairing->g1_x_only_bytes_length() == 64);
+    BOOST_TEST(pairing->g1_compressed_bytes_length() == 65);
+    BOOST_TEST(pairing->g2_bytes_length() == 128);
+    BOOST_TEST(pairing->g2_x_only_bytes_length() == 64);
+    BOOST_TEST(pairing->g2_compressed_bytes_length() == 65);
+    BOOST_TEST(pairing->gt_bytes_length() == 128);
+    BOOST_TEST(pairing->zr_bytes_length() == 20);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
